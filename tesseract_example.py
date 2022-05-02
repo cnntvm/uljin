37249d77a0790c1c01b931155fc06ddeb29b9c15
donghee <cnntvm@gmail.com>
2022-05-02 21:58:23 +0900

    first commit

--- tesseract_example.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-import pytesseract
-
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-text = pytesseract.image_to_string(Image.open("amos.png"))
-print(text)
